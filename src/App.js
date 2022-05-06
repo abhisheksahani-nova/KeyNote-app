ea750dee2ebd8998b70b539007506e9a46a40eb2
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-05-06 10:42:41 +0530

    Add sidebar styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Navbar } from "./components/index";
+import { Navbar, Sidebar } from "./components/index";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <section class="d-flex">
+        <Sidebar />
+      </section>
     </div>
   );
 }
