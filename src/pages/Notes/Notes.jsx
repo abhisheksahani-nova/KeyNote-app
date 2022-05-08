177f9416a3e572b0a0609d346a399ca9fb78f904
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-05-08 10:41:58 +0530

    Add note taking input box

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,9 +1,12 @@
-import React,{useEffect} from "react";
+import React, { useState } from "react";
 import { Navbar, Sidebar, NoteCard } from "../../components/index";
 import "./Notes.css";
 import { useNotes } from "../../context/notes-context";
+import TextareaAutosize from "react-textarea-autosize";
 
 function Notes() {
+  const [openCreateNote, setOpenCreateNote] = useState(false);
+
   const { getNotes } = useNotes();
   const token = localStorage.getItem("token");
 
@@ -14,15 +17,49 @@ function Notes() {
         <Sidebar />
 
         <div className="notes-container">
-          <div className="note-inp-container mb-2">
-            {/* <input
-              className="nav_searchBar add-note-inp"
-              type="text"
-              placeholder="Take a note..."
-            /> */}
-            <div>
-              
-            </div>
+          <div className="d-flex mb-2">
+            {openCreateNote ? (
+              <div className="create-note-container">
+                <div className="d-flex title-inp-container mb-2">
+                  <input
+                    className="note-title-inp"
+                    type="text"
+                    placeholder="Title"
+                  />
+                  <i className="fa-solid fa-thumbtack"></i>
+                </div>
+
+                <TextareaAutosize
+                  className="create-note-textarea"
+                  placeholder="Take a note..."
+                />
+
+                <div className="d-flex note-footer mt-2">
+                  <div className="d-flex note-footer note-icons-container create-note-footer-icons-container">
+                    <i className="fa-solid fa-pencil"></i>
+                    <i className="fa-solid fa-box-archive"></i>
+                    <i className="fa-solid fa-trash-can"></i>
+                  </div>
+                  <div className="d-flex note-footer note-label-priority-container create-note-btn-container">
+                    <button
+                      onClick={() => setOpenCreateNote((prev) => !prev)}
+                      className="btn btn-outline"
+                    >
+                      Close
+                    </button>
+                    <button className="btn pri-btn-style">Save</button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <input
+                onClick={() => setOpenCreateNote((prev) => !prev)}
+                className="nav_searchBar add-note-inp"
+                type="text"
+                placeholder="Take a note..."
+              />
+            )}
+
             <button className="btn pri-btn-style">Filter</button>
           </div>
           <div>
